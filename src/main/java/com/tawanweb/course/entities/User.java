f13955175a09e4f0b305e08fe6eba36021f4e8f3
Tawan Silva <tawan.tls43@gmail.com>
2022-07-31 20:23:30 -0300

    H2 database, test profile, JPA

--- src/main/java/com/tawanweb/course/entities/User.java
+++ src/main/java/com/tawanweb/course/entities/User.java
@@ -1,11 +1,18 @@
 package com.tawanweb.course.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "Person")
 public class User implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
@@ -13,9 +20,7 @@ public class User implements Serializable {
 	private String password;
 	
 	public User() {
-		
 	}
-
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
@@ -24,52 +29,43 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getPhone() {
 		return phone;
 	}
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -79,8 +75,11 @@ public class User implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
 }
