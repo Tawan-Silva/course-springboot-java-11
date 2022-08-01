6eff83536feb77ea3046fde28159f109e54d28ce
Tawan Silva <tawan.tls43@gmail.com>
2022-08-01 17:23:47 -0300

    Exception handling - findById

---
+++ src/main/java/com/tawanweb/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.tawanweb.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Rosource not foud. Id " + id);
+		
+	}
+	
+}
